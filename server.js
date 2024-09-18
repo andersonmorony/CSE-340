411f0410fda0ec1e5fdab63bb587988253a09ef9
Anderson Moroni <andersonmoroni92@gmail.com>
2024-09-18 18:36:25 -0300

    feat: add changes of week one

--- server.js
+++ server.js
@@ -6,14 +6,21 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+// View engine and templates
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
 
 /* ***********************
  * Local Server Information
@@ -27,4 +34,9 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+//Index route
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
 })
